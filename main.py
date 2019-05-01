fcf8f40795692cecdc6b112e4b1c47ad94105970
sasitani <sasitani_611@yahoo.co.jp>
2019-05-01 10:22:02 +0900

    マツコ類似機能追加

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+import requests
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -10,6 +11,8 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
+from io import BytesIO
+import json
 
 app = Flask(__name__)
 
@@ -19,6 +22,29 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+url_detect = 'https://japanwest.api.cognitive.microsoft.com/face/v1.0/detect'
+url_similar = 'https://japanwest.api.cognitive.microsoft.com/face/v1.0/findsimilars'
+
+headers_detect = {
+    'Content-Type': 'application/octet-stream',
+    'Ocp-Apim-Subscription-Key': ''
+}
+
+headers_similar = {
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': ''
+}
+
+params = {
+    'returnFaceId': 'true',  # The default value is true.
+    'returnFaceLandmarks': 'false', # The default value is false.
+    'returnFaceAttributes': 'age,gender', # age, gender, headPose, smile, facialHair, and glasses.
+}
+
+body = {
+    "mode": "matchFace"
+}
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -37,12 +63,32 @@ def callback():
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='顔画像を送信しなさい'))
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image(event):
+    message_id = event.message.id
+    message_content = line_bot_api.get_message_content(message_id)
+    image = BytesIO(message_content.content)
+
+    r_1 = requests.post(url_detect, headers = headers_detect, params = params, data = open('matsuko.jpg','rb'))
+    r_2 = requests.post(url_detect , headers = headers_detect, params = params, data = image)
+    faceId = r_1.json()[0]['faceId']
+    #line_bot_api.reply_message(event.reply_token,TextSendMessage(text='あなたの年齢は{}'.format(r_2.json()[0])))
+
+    try:
+        faceIds = r_2.json()[0]['faceId']
+        body['faceId'] = faceId
+        body['faceIds'] = [faceIds]
+        r_find_similar = requests.post(url_similar, headers = headers_similar, data = json.dumps(body)).json()[0]['confidence']
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='私との類似度は{}'.format(r_find_similar)))
+
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='顔が検出できなかったですよ'))
 
 
 if __name__ == "__main__":
